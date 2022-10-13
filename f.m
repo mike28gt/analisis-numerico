a6697d5059c25987e0e765d00d82c9357e75d540
mike28gt <miguelalejandro28@gmail.com>
2022-10-13 02:27:10 -0600

    Se agrega función para método de Bisección y de la Falsa Posición

---
+++ f.m
@@ -0,0 +1,32 @@
+## Copyright (C) 2022 Miguel Catalan
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {@var{retval} =} f (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Miguel Catalan <miguelcatalan@Miguels-MacBook-Air.local>
+## Created: 2022-10-05
+
+function retval = f (c)
+  m = 68.1;
+  v = 40;
+  t = 10;
+  g = 9.81;
+
+  retval = ((g * m) / c) * (1 - exp(-(c/m)* t)) - v;
+endfunction
