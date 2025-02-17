9cb3cb3da75c97fc3c7695b2e2376a6b8565e02b
Miguel Catalan <miguelcatalan@MacBook-Pro.local>
2025-02-16 23:47:04 -0600

    Se agregan nuevos ejemplos.

---
+++ 2025/Clase11.m
@@ -0,0 +1,9 @@
+x = input("Ingrese el valor de x: ");
+y = input("Ingrese el valor de y: ");
+if x >= 0 && y ~= 0
+    fx = sqrt(16 * x) / y;
+    disp(fx);
+else
+    disp("El valor de x o y es inválido.");
+end
+
