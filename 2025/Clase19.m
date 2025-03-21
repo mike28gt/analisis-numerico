a02f5312bb682e9eb9c9b25bb5d24966bc88bcf4
Miguel Catalan <miguelcatalan@MacBook-Pro.local>
2025-03-20 21:49:39 -0600

    Ejemplo acumuladores.

---
+++ 2025/Clase19.m
@@ -0,0 +1,16 @@
+%1 + 2 + 3 + 4 + 5 + 6 + ... + 10
+
+contador = 1;
+acumulador = 0;
+
+while contador <= 10
+    acumulador = acumulador + contador;
+    contador = contador + 1;
+end
+
+disp(acumulador);
+
+
+
+% 1 * 2 * 3 * 4 * 5 = 
+% acumulador = acumulador * contador
