829167bc576a04e382d7af1bb96fd15f7e9076b5
Miguel Catalan <miguelcatalan@MacBook-Pro.local>
2025-05-29 20:07:41 -0600

    Ejemplo modularidad.

---
+++ 2025/Clase26_01.m
@@ -0,0 +1,7 @@
+numero_1 = input("Ingrese el primer número: ");
+numero_2 = input("Ingrese el segundo número: ");
+numero_3 = input("Ingrese el tercer número: ");
+
+promedio = calcular_promedio(numero_1, numero_2, numero_3);
+
+fprintf("El promedio es de %.2f\n", promedio);
---
+++ 2025/calcular_promedio.m
@@ -0,0 +1,3 @@
+function res = calcular_promedio(n1, n2, n3)
+    res = (n1 + n2 + n3) / 3;
+end
